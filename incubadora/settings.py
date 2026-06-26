48f00fd36ac408f9a957668cd669f242e8d607d8
Misifus13 <wilfred130594@gmail.com>
2026-06-25 23:36:47 -0400

    Update settings.py

--- incubadora/settings.py
+++ incubadora/settings.py
@@ -76,6 +76,9 @@ WSGI_APPLICATION = 'incubadora.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL")
