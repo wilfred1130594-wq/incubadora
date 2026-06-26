21e04a8d77e6785fc1b42378bfc85eb37b26b47d
Misifus13 <wilfred130594@gmail.com>
2026-06-25 23:47:47 -0400

    Configurar STATIC_ROOT y WhiteNoise

--- incubadora/settings.py
+++ incubadora/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+<<<<<<< HEAD
 STATIC_ROOT = BASE_DIR / "staticfiles"
+=======
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+>>>>>>> ae93aac (Configurar STATIC_ROOT y WhiteNoise)
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
