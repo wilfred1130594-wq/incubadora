aed25cdea63383b44af00f3c86b46727a64c2f47
Misifus13 <wilfred130594@gmail.com>
2026-06-26 13:56:08 -0400

    Añadir funcionalidad de exportación de datos a CSV

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/registro/', views.registro_api),
     path('actualizar-config/', views.actualizar_config_api, name='actualizar_config'),
     path('detener-incubacion/', views.detener_incubacion_api, name='detener_incubacion'),
+    path('exportar-csv/', views.exportar_csv_api, name='exportar_csv'),
 ]
